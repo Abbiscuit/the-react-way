ab8b4eaaa1d289e2fb9229fb1e096386c45f212b
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-07-13 08:58:06 +0900

    Add appHeader component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import AppHeader from './components/appHeader/appHeader.component';
+import SearchBar from './components/searchBar/searchBar.component';
+import ProductTable from './components/productTable/productTable.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppHeader />
+        <SearchBar />
+        <ProductTable />
+      </div>
+    );
+  }
 }
 
 export default App;
